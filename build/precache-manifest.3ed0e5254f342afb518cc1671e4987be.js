7e85696510ba3f8fc4b900d0f58df60a67fcf507
Vincent-Jabonete <53134258+Vincent-Jabonete@users.noreply.github.com>
2019-09-08 10:55:26 +0800

    Done

---
+++ build/precache-manifest.3ed0e5254f342afb518cc1671e4987be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50b7947a5ac8edcc16d1545dfb43f76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb00b1f5f94e1407f754",
+    "url": "/static/css/main.99f087ff.chunk.css"
+  },
+  {
+    "revision": "9512bf34cc797fdacdca",
+    "url": "/static/js/2.dee05dd3.chunk.js"
+  },
+  {
+    "revision": "fb00b1f5f94e1407f754",
+    "url": "/static/js/main.25be7a7b.chunk.js"
+  },
+  {
+    "revision": "0c1400ec95fcc8fa04a7",
+    "url": "/static/js/runtime~main.5134c318.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
